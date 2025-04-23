7013a23c895d3c8e5f131d684f85c3fa7e515cac
kapil56J <kapiljadhav5656@gmail.com>
2025-04-23 15:40:08 +0530

    JS Preparation

--- DSA/WordOccurrence.js
+++ DSA/WordOccurrence.js
@@ -48,6 +48,20 @@ function countRepeatedWords(sentence, bannedWord) {
   bannedWord = "this";
   countRepeatedWords(sentence, bannedWord);
   
+// ============================================================================
 
-
+// occurence of each element in an array
+const array2 = [1, 2, 2, 3, 4, 5, 6, 6 , 7, 8, 9, 10];
+const occurrence = {};
+array2.forEach(value => {
+  if (occurrence[value]) {
+    occurrence[value] += 1;
+  }
+  else {
+    occurrence[value] = 1;
+  }
+});
+// explaination: forEacch takes a callback function and iterates over each element of the array. It checks if the value already exists in the occurrence object. If it does, it increments the count by 1. If it doesn't, it initializes the count to 1.
+console.log('occurrence', occurrence);
+// Output: { '1': 1, '2': 2, '3': 1, '4': 1, '5': 1, '6': 2, '7': 1, '8': 1, '9': 1, '10': 1 }
 
