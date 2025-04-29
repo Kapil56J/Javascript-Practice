c0aa2b60a321d04ff88670019044189bc3d18b61
Kapil Jadhav <103952813+Kapil56J@users.noreply.github.com>
2025-04-29 23:19:48 +0530

    Update FibonacciSeries.js

--- DSA/FibonacciSeries.js
+++ DSA/FibonacciSeries.js
@@ -1,1 +1,11 @@
 // program to generate fibonacci series up to n terms
+
+const number = parseInt(prompt('Enter the number of terms: '));
+let n1 = 0, n2 = 1, nextNumber;
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextNumber = n1 + n2;
+    n1 = n2;
+    n2 = nextNumber;
+};
