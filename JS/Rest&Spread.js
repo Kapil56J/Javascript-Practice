b84f69ca32f48d97804d32182cb0a208403e75b7
Kapil Jadhav <103952813+Kapil56J@users.noreply.github.com>
2025-05-23 10:50:45 +0530

    Update Rest&Spread.js

--- JS/Rest&Spread.js
+++ JS/Rest&Spread.js
@@ -12,7 +12,8 @@ greet('Kapil', 'Ram', 'Sita', 'Laxman');
 // Others: [ 'Ram', 'Sita', 'Laxman' ]
 
 // Spread Operator:
-// Used to spread (expand) elements from an array/object into individual elements.
+// Used to spread (expand) elements from an array/object into individual elements also create the new array by combining existing array or adding new elements
+// also cloning/update the oject.
 const fruits = ['apple', 'banana'];
 const moreFruits = ['mango', ...fruits, 'orange'];
 
@@ -29,3 +30,13 @@ const updateUser = {...user, age: 26};
 console.log(updateUser);
 // Output
 // { name: 'Kapil', age: 26 }
+
+
+// Destructuring in JavaScript provides a concise way to extract values from arrays or objects and assign them to distinct variables
+
+const colors = ['red', 'green', 'blue'];
+const [first, second, third] = colors;
+
+console.log(first);  // Output: red
+console.log(second); // Output: green
+console.log(third);  // Output: blue
